1fe63e22185b7aaf33d912d1606c03332b63af52
Pontus Enmark <pontus@wka.se>
2013-11-29 11:28:40 +0100

    Don’t auto-add .form-control to checkboxes and textfields inside FormElementContainers

--- gwtbootstrap3/src/main/java/com/svenjacobs/gwtbootstrap3/client/ui/base/form/FormElementContainer.java
+++ gwtbootstrap3/src/main/java/com/svenjacobs/gwtbootstrap3/client/ui/base/form/FormElementContainer.java
@@ -20,41 +20,41 @@ package com.svenjacobs.gwtbootstrap3.client.ui.base.form;
  * #L%
  */
 
-import com.google.gwt.user.client.ui.*;
+import com.google.gwt.user.client.ui.ComplexPanel;
+import com.google.gwt.user.client.ui.FileUpload;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.Widget;
 import com.svenjacobs.gwtbootstrap3.client.ui.HasResponsiveness;
 import com.svenjacobs.gwtbootstrap3.client.ui.base.helper.StyleHelper;
 import com.svenjacobs.gwtbootstrap3.client.ui.constants.Styles;
 
 /**
- * Base class for {@link com.svenjacobs.gwtbootstrap3.client.ui.Form} related widgets that may contain form input
- * elements.
+ * Base class for {@link com.svenjacobs.gwtbootstrap3.client.ui.Form} related
+ * widgets that may contain form input elements.
  * <p/>
- * Input elements styleName is adjusted during {@link #add(com.google.gwt.user.client.ui.Widget)}.
- *
+ * Input elements styleName is adjusted during
+ * {@link #add(com.google.gwt.user.client.ui.Widget)}.
+ * 
  * @author Sven Jacobs
  * @author Joshua Godi
  */
 public abstract class FormElementContainer extends ComplexPanel implements HasResponsiveness {
 
-    @Override
-    public void add(final Widget w) {
+	@Override
+	public void add(final Widget w) {
+		if (w instanceof ListBox || w instanceof FileUpload) {
+			w.addStyleName(Styles.FORM_CONTROL);
+		}
+		add(w, getElement());
+	}
 
-        if (w instanceof TextBoxBase || w instanceof SimpleCheckBox || w instanceof CheckBox || w instanceof ListBox ||
-                w instanceof FileUpload) {
+	@Override
+	public void setVisibleOn(final String deviceSizeString) {
+		StyleHelper.setVisibleOn(this, deviceSizeString);
+	}
 
-            w.addStyleName(Styles.FORM_CONTROL);
-        }
-
-        add(w, getElement());
-    }
-
-    @Override
-    public void setVisibleOn(String deviceSizeString) {
-        StyleHelper.setVisibleOn(this, deviceSizeString);
-    }
-
-    @Override
-    public void setHiddenOn(String deviceSizeString) {
-        StyleHelper.setHiddenOn(this, deviceSizeString);
-    }
+	@Override
+	public void setHiddenOn(final String deviceSizeString) {
+		StyleHelper.setHiddenOn(this, deviceSizeString);
+	}
 }
