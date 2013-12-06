667026874f31605a0844c7fbad766b037dece682
Pontus Enmark <pontus@wka.se>
2013-12-06 14:51:28 +0100

    Fix license header

--- gwtbootstrap3/src/main/java/com/svenjacobs/gwtbootstrap3/client/shared/event/TooltipHiddenEvent.java
+++ gwtbootstrap3/src/main/java/com/svenjacobs/gwtbootstrap3/client/shared/event/TooltipHiddenEvent.java
@@ -21,47 +21,45 @@ import com.svenjacobs.gwtbootstrap3.client.ui.Tooltip;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * #Limport com.google.gwt.event.shared.GwtEvent;
- import com.google.gwt.user.client.Event;
- import com.svenjacobs.gwtbootstrap3.client.ui.Tooltip;
- oltip;
+ * #L%
+ */
 
- /**
+/**
  * @author Pontus Enmark
  */
 public class TooltipHiddenEvent extends GwtEvent<TooltipHiddenHandler> implements TooltipEvent {
 
-	private static final Type<TooltipHiddenHandler> TYPE = new Type<TooltipHiddenHandler>();
+    private static final Type<TooltipHiddenHandler> TYPE = new Type<TooltipHiddenHandler>();
 
-	private final Tooltip tooltip;
-	private final Event nativeEvent;
+    private final Tooltip tooltip;
+    private final Event nativeEvent;
 
-	public static Type<TooltipHiddenHandler> getType() {
-		return TYPE;
-	}
+    public static Type<TooltipHiddenHandler> getType() {
+        return TYPE;
+    }
 
-	public TooltipHiddenEvent(final Tooltip tooltip, final Event nativeEvent) {
-		this.tooltip = tooltip;
-		this.nativeEvent = nativeEvent;
-	}
+    public TooltipHiddenEvent(final Tooltip tooltip, final Event nativeEvent) {
+        this.tooltip = tooltip;
+        this.nativeEvent = nativeEvent;
+    }
 
-	@Override
-	public Tooltip getTooltip() {
-		return tooltip;
-	}
+    @Override
+    public Tooltip getTooltip() {
+        return tooltip;
+    }
 
-	@Override
-	public Event getNativeEvent() {
-		return nativeEvent;
-	}
+    @Override
+    public Event getNativeEvent() {
+        return nativeEvent;
+    }
 
-	@Override
-	public Type<TooltipHiddenHandler> getAssociatedType() {
-		return TYPE;
-	}
+    @Override
+    public Type<TooltipHiddenHandler> getAssociatedType() {
+        return TYPE;
+    }
 
-	@Override
-	protected void dispatch(final TooltipHiddenHandler handler) {
-		handler.onHidden(this);
-	}
+    @Override
+    protected void dispatch(final TooltipHiddenHandler handler) {
+        handler.onHidden(this);
+    }
 }
