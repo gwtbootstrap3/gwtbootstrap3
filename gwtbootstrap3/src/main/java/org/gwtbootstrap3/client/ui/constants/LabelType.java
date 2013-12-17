47fa98003ac3b816c111bb134d8c7be35525cb0d
Sven Jacobs <mail@svenjacobs.com>
2013-12-17 08:08:42 +0100

    Updated license headers

--- gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/constants/LabelType.java
+++ gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/constants/LabelType.java
@@ -4,7 +4,7 @@ package org.gwtbootstrap3.client.ui.constants;
  * #%L
  * GwtBootstrap3
  * %%
- * Copyright (C) 2013 Sven Jacobs
+ * Copyright (C) 2013 GwtBootstrap3
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
