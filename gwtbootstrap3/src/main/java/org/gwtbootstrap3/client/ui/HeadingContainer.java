aa677c333ee91773440ae17f573cef319d411377
jgodi <joshuagodi@gmail.com>
2014-01-20 08:47:44 -0600

    Adding in a HeadingContainer that let's users to the h(1-6) tag as a container to create in-line layouts that HTML code allows. (Issue #40).

---
+++ gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/HeadingContainer.java
@@ -0,0 +1,58 @@
+package org.gwtbootstrap3.client.ui;
+
+/*
+ * #%L
+ * GwtBootstrap3
+ * %%
+ * Copyright (C) 2013 - 2014 GwtBootstrap3
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import com.google.gwt.uibinder.client.UiConstructor;
+import com.google.gwt.user.client.DOM;
+import org.gwtbootstrap3.client.ui.base.ComplexWidget;
+import org.gwtbootstrap3.client.ui.base.helper.StyleHelper;
+
+/**
+ * Container with the h(1-6) tag so that you can do layouts like:
+ * <p/>
+ * <HeadingContainer>
+ * <Icon/>
+ * <Text/>
+ * </HeadingContainer>
+ * <p/>
+ * Please note, this doesn't enforce any requirements on the children. It is up to
+ * you to make sure you produce valid HTML code.
+ *
+ * @author godi
+ */
+public class HeadingContainer extends ComplexWidget implements HasResponsiveness {
+
+    @UiConstructor
+    public HeadingContainer(final int size) {
+        setElement(DOM.createElement("h" + size));
+        assert size > 0 && size < 7 : "Wrong heading size (must be between 1 and 6)";
+    }
+
+    @Override
+    public void setVisibleOn(final String deviceSizeString) {
+        StyleHelper.setVisibleOn(this, deviceSizeString);
+    }
+
+    @Override
+    public void setHiddenOn(final String deviceSizeString) {
+        StyleHelper.setHiddenOn(this, deviceSizeString);
+    }
+}
