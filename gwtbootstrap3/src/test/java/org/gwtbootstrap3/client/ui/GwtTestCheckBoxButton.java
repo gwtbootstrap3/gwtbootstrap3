bed8e42c2efd09bfdbb9fd03fd1808737a800bf7
Arlo O'Keeffe <mailtoarlo@gmail.com>
2014-09-24 23:17:19 +0200

    Add a simple test of CheckBoxButton

---
+++ gwtbootstrap3/src/test/java/org/gwtbootstrap3/client/ui/GwtTestCheckBoxButton.java
@@ -0,0 +1,48 @@
+package org.gwtbootstrap3.client.ui;
+
+/*
+ * #%L
+ * GwtBootstrap3
+ * %%
+ * Copyright (C) 2013 - 2014 GwtBootstrap3
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.gwtbootstrap3.client.ui.CheckBoxButton;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class GwtTestCheckBoxButton extends GWTTestCase {
+
+    /**
+     * Specifies a module to use when running this test case. The returned
+     * module must include the source for this class.
+     *
+     * @see com.google.gwt.junit.client.GWTTestCase#getModuleName()
+     */
+    @Override
+    public String getModuleName() {
+        return "org.gwtbootstrap3.GwtBootstrap3";
+    }
+
+    public void testLabel() {
+        final String label = "label";
+
+        final CheckBoxButton button = new CheckBoxButton(label);
+
+        assertEquals(label, button.getText());
+    }
+
+}
