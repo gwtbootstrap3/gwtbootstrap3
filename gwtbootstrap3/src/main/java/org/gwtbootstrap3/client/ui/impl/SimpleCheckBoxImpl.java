0d8d2163eecc78d111d3069c620aa5072009b31e
Joshua Godi <Joshua.Godi@nisc.coop>
2014-12-08 14:25:56 -0600

    Fixes #37 - Scrolling when navigating through the demo

--- gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/impl/SimpleCheckBoxImpl.java
+++ gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/impl/SimpleCheckBoxImpl.java
@@ -9,9 +9,9 @@ package org.gwtbootstrap3.client.ui.impl;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,11 +20,10 @@ package org.gwtbootstrap3.client.ui.impl;
  * #L%
  */
 
-import org.gwtbootstrap3.client.ui.SimpleCheckBox;
-
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import org.gwtbootstrap3.client.ui.SimpleCheckBox;
 
 public class SimpleCheckBoxImpl {
 
