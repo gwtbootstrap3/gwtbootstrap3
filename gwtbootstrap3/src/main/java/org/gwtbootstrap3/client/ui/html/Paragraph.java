a0f58c9cf2aa3b57df95e3dc4ebbf38c5138de3d
Arlo O'Keeffe <mailtoarlo@gmail.com>
2015-01-17 00:32:10 +0100

    Call this("") to reduce duplicate code

--- gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/html/Paragraph.java
+++ gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/html/Paragraph.java
@@ -37,7 +37,7 @@ public class Paragraph extends HTMLPanel implements HasAlignment, HasEmphasis {
     private final HTMLMixin<Paragraph> textMixin = new HTMLMixin<Paragraph>(this);
 
     public Paragraph() {
-        super(ParagraphElement.TAG, "");
+        this("");
     }
 
     public Paragraph(final String html) {
