d402d70152f50cca7d8f7ff5e0f42ca6ea8c1669
Xiaodong Sun <xsun@wallix.com>
2015-01-23 15:45:47 +0100

    Git merge

--- gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/GwtBootstrap3ClientBundle.java
+++ gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/GwtBootstrap3ClientBundle.java
@@ -9,9 +9,9 @@ package org.gwtbootstrap3.client;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,9 +32,9 @@ public interface GwtBootstrap3ClientBundle extends ClientBundle {
 
     static final GwtBootstrap3ClientBundle INSTANCE = GWT.create(GwtBootstrap3ClientBundle.class);
 
-    @Source("resource/js/jquery-1.11.2.min.js")
+	@Source("resource/js/jquery-1.11.2.min.cache.js")
     TextResource jQuery();
 
-    @Source("resource/js/bootstrap-3.3.1.min.cache.js")
+	@Source("resource/js/bootstrap-3.3.2.min.cache.js")
     TextResource bootstrap();
 }
