72856bf01c7612b67d01fb67dcd9003ad4ca931a
Arlo Louis O'Keeffe <mailtoarlo@gmail.com>
2015-02-03 22:32:45 +0100

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+set -e
+if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
+  echo "<settings><servers><server><id>ossrh</id><username>\${env.OSSRH_USER}</username><password>\${env.OSSRH_PASS}</password></server></servers></settings>" > ~/settings.xml
+  mvn deploy --settings ~/settings.xml
+fi
