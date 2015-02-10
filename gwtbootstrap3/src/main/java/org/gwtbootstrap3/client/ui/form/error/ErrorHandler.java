282a674d5fd82cc543277baed512e1a10f7ba58c
Steven Jardine <steve@mjnservices.com>
2015-02-10 11:52:14 -0700

    Added/fixed file headers and javadoc.

--- gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/form/error/ErrorHandler.java
+++ gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/form/error/ErrorHandler.java
@@ -8,7 +8,7 @@ import com.google.gwt.editor.client.EditorError;
  * #%L
  * GwtBootstrap3
  * %%
- * Copyright (C) 2013 - 2015 GwtBootstrap3
+ * Copyright (C) 2015 GwtBootstrap3
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
