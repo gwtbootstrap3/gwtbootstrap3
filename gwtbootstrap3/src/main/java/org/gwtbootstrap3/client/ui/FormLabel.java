d5c2fe989c29c488a65e0f77c41bf68e07e8915e
Steven Jardine <steve@mjnservices.com>
2015-02-12 17:13:31 -0700

    Added required star when the label is marked as a "required" label.

--- gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/FormLabel.java
+++ gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/FormLabel.java
@@ -4,7 +4,7 @@ package org.gwtbootstrap3.client.ui;
  * #%L
  * GwtBootstrap3
  * %%
- * Copyright (C) 2013 GwtBootstrap3
+ * Copyright (C) 2013 - 2015 GwtBootstrap3
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,18 +22,79 @@ package org.gwtbootstrap3.client.ui;
 
 import org.gwtbootstrap3.client.ui.base.AbstractTextWidget;
 import org.gwtbootstrap3.client.ui.constants.Attributes;
+import org.gwtbootstrap3.client.ui.constants.ElementTags;
+import org.gwtbootstrap3.client.ui.constants.IconType;
 import org.gwtbootstrap3.client.ui.constants.Styles;
 
 import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.dom.client.DomEvent;
 
 /**
  * @author Sven Jacobs
+ * @author Steven Jardine
  */
 public class FormLabel extends AbstractTextWidget {
 
+    private Element iconElement = null;
+
+    private boolean required = false;
+
+    /**
+     * Constructor.
+     */
     public FormLabel() {
         super(Document.get().createLabelElement());
         setStyleName(Styles.CONTROL_LABEL);
+        addHandler(new ChangeHandler() {
+            @Override
+            public void onChange(ChangeEvent event) {
+                String html = getHTML();
+                if (!required || html == null || "".equals(html)) {
+                    if (iconElement != null) {
+                        iconElement.removeFromParent();
+                    }
+                } else {
+                    if (iconElement == null) {
+                        iconElement = createIconElement();
+                    }
+                    getElement().appendChild(iconElement);
+                }
+            }
+        }, ChangeEvent.getType());
+    }
+
+    /**
+     * @return a new icon element. We only create this when {@link #iconElement} is null or the
+     *         {@link #required} has changed.
+     */
+    protected Element createIconElement() {
+        Element e = Document.get().createElement(ElementTags.I);
+        e.addClassName(Styles.FONT_AWESOME_BASE);
+        e.addClassName(IconType.STAR.getCssName());
+        e.addClassName(Styles.HAS_ERROR);
+        Style s = e.getStyle();
+        s.setFontSize(6, Unit.PX);
+        s.setPaddingLeft(2, Unit.PX);
+        s.setPaddingRight(5, Unit.PX);
+        s.setColor("#b94a48");
+
+        Element sup = Document.get().createElement("sup");
+        sup.addClassName(Styles.HAS_ERROR);
+        sup.appendChild(e);
+
+        return sup;
+    }
+
+    /**
+     * @return does this label show required?
+     */
+    public boolean getRequired() {
+        return required;
     }
 
     public void setFor(final String f) {
@@ -43,4 +104,27 @@ public class FormLabel extends AbstractTextWidget {
             getElement().removeAttribute(Attributes.FOR);
         }
     }
+
+    /** {@inheritDoc} */
+    @Override
+    public void setHTML(final String html) {
+        super.setHTML(html);
+        DomEvent.fireNativeEvent(Document.get().createChangeEvent(), this);
+    }
+
+    /**
+     * @param should this label show as required?
+     */
+    public void setRequired(boolean required) {
+        this.required = required;
+        DomEvent.fireNativeEvent(Document.get().createChangeEvent(), this);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void setText(String text) {
+        super.setText(text);
+        DomEvent.fireNativeEvent(Document.get().createChangeEvent(), this);
+    }
+
 }
