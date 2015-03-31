3fbe64c236cc167d2e828c99f859be14948202b1
Drew Spencer <slugmandrew@gmail.com>
2015-03-31 17:33:39 +0100

    ValueListBox improvements

--- gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/ValueListBox.java
+++ gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/ValueListBox.java
@@ -20,13 +20,29 @@ package org.gwtbootstrap3.client.ui;
  * #L%
  */
 
+import java.util.List;
 
+import org.gwtbootstrap3.client.ui.base.HasId;
+import org.gwtbootstrap3.client.ui.base.mixin.BlankValidatorMixin;
+import org.gwtbootstrap3.client.ui.base.mixin.ErrorHandlerMixin;
+import org.gwtbootstrap3.client.ui.base.mixin.IdMixin;
+import org.gwtbootstrap3.client.ui.form.error.ErrorHandler;
+import org.gwtbootstrap3.client.ui.form.error.ErrorHandlerType;
+import org.gwtbootstrap3.client.ui.form.error.HasErrorHandler;
+import org.gwtbootstrap3.client.ui.form.validator.HasBlankValidator;
+import org.gwtbootstrap3.client.ui.form.validator.HasValidators;
+import org.gwtbootstrap3.client.ui.form.validator.ValidationChangedEvent.ValidationChangedHandler;
+import org.gwtbootstrap3.client.ui.form.validator.Validator;
+
+import com.google.gwt.editor.client.EditorError;
+import com.google.gwt.editor.client.HasEditorErrors;
 import com.google.gwt.text.shared.Renderer;
 import com.google.gwt.user.client.ui.HasConstrainedValue;
 import com.google.gwt.user.client.ui.HasName;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.view.client.ProvidesKey;
 import com.google.gwt.view.client.SimpleKeyProvider;
+import com.google.web.bindery.event.shared.HandlerRegistration;
 
 /**
  * Implementation of {@link HasConstrainedValue} based on a
@@ -35,9 +51,18 @@ import com.google.gwt.view.client.SimpleKeyProvider;
  * A {@link Renderer Renderer<T>} is used to get user-presentable strings to
  * display in the select element.
  *
- * @param <T> the value type
+ * @param <T>
+ *            the value type
  */
-public class ValueListBox<T> extends com.google.gwt.user.client.ui.ValueListBox<T> implements HasName {
+public class ValueListBox<T> extends com.google.gwt.user.client.ui.ValueListBox<T> implements HasName, HasId,
+        HasEditorErrors<T>, HasErrorHandler, HasValidators<T>, HasBlankValidator<T> {
+
+    private final IdMixin<ValueListBox<T>> idMixin = new IdMixin<ValueListBox<T>>(this);
+
+    private final ErrorHandlerMixin<T> errorHandlerMixin = new ErrorHandlerMixin<T>(this);
+
+    private final BlankValidatorMixin<ValueListBox<T>, T> validatorMixin = new BlankValidatorMixin<ValueListBox<T>, T>(
+            this, errorHandlerMixin.getErrorHandler());
 
     public ValueListBox(final Renderer<T> renderer) {
         this(renderer, new SimpleKeyProvider<T>());
@@ -60,5 +85,94 @@ public class ValueListBox<T> extends com.google.gwt.user.client.ui.ValueListBox<
     @Override
     public String getName() {
         return ((ListBox) getWidget()).getName();
+    }
+
+    @Override
+    public HandlerRegistration addValidationChangedHandler(ValidationChangedHandler handler) {
+        return validatorMixin.addValidationChangedHandler(handler);
+    }
+
+    @Override
+    public boolean getAllowBlank() {
+        return validatorMixin.getAllowBlank();
+    }
+
+    @Override
+    public void setAllowBlank(boolean allowBlank) {
+        validatorMixin.setAllowBlank(allowBlank);
+    }
+
+    @Override
+    public void addValidator(Validator<T> validator) {
+        validatorMixin.addValidator(validator);
+    }
+
+    @Override
+    public boolean getValidateOnBlur() {
+        return validatorMixin.getValidateOnBlur();
+    }
+
+    @Override
+    public void reset() {
+        setValue(null);
+        validatorMixin.reset();
+    }
+
+    @Override
+    public void setValidateOnBlur(boolean validateOnBlur) {
+        validatorMixin.setValidateOnBlur(validateOnBlur);
+    }
+
+    @Override
+    public void setValidators(Validator<T>... validators) {
+        validatorMixin.setValidators(validators);
+    }
+
+    @Override
+    public boolean validate() {
+        return validatorMixin.validate();
+    }
+
+    @Override
+    public boolean validate(boolean show) {
+        return validatorMixin.validate(show);
+    }
+
+    @Override
+    public ErrorHandler getErrorHandler() {
+        return errorHandlerMixin.getErrorHandler();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void setErrorHandler(ErrorHandler handler) {
+        errorHandlerMixin.setErrorHandler(handler);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public ErrorHandlerType getErrorHandlerType() {
+        return errorHandlerMixin.getErrorHandlerType();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void setErrorHandlerType(ErrorHandlerType type) {
+        errorHandlerMixin.setErrorHandlerType(type);
+    }
+
+    @Override
+    public void showErrors(List<EditorError> errors) {
+        errorHandlerMixin.showErrors(errors);
+    }
+
+    @Override
+    public void setId(final String id) {
+        idMixin.setId(id);
+    }
+
+    @Override
+    public String getId() {
+        return idMixin.getId();
     }
 }
