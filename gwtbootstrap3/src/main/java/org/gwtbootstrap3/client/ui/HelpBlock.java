51d74ee00ce6c5aa2c94c193ec55c868fbee80c2
Steven Jardine <steve@mjnservices.com>
2015-07-27 15:41:30 -0600

    Added TooltipHelpBlock which displays a ToolTip as the message for a validation error.

--- gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/HelpBlock.java
+++ gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/HelpBlock.java
@@ -33,80 +33,116 @@ import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.DomEvent;
 
 /**
+ * Help block. Call {@link #setError(String)} to set ther error state of this
+ * {@link HelpBlock}.
+ * 
  * @author Joshua Godi
  * @author Steven Jardine
  */
 public class HelpBlock extends AbstractTextWidget {
 
-    private Element iconElement = null;
+	private boolean error = false;
 
-    private IconType iconType = null;
+	private Element iconElement = null;
 
-    public HelpBlock() {
-        super(Document.get().createSpanElement());
-        setStyleName(Styles.HELP_BLOCK);
-        addHandler(new ChangeHandler() {
-            @Override
-            public void onChange(ChangeEvent event) {
-                if (iconElement != null) {
-                    iconElement.removeFromParent();
-                }
-                String html = getHTML();
-                if (html != null && !"".equals(html) && iconType != null) {
-                    iconElement = createIconElement();
-                    getElement().insertFirst(iconElement);
-                }
-            }
-        }, ChangeEvent.getType());
-    }
+	private IconType iconType = null;
 
-    /**
-     * @return a new icon element. We only create this when {@link #iconElement} is null or the
-     *         {@link #iconType} has changed.
-     */
-    protected Element createIconElement() {
-        Element e = Document.get().createElement(ElementTags.I);
-        e.addClassName(Styles.FONT_AWESOME_BASE);
-        e.addClassName(iconType.getCssName());
-        e.getStyle().setPaddingRight(5, Unit.PX);
-        return e;
-    }
+	/**
+	 * Constructor.
+	 */
+	public HelpBlock() {
+		super(Document.get().createSpanElement());
+		setStyleName(Styles.HELP_BLOCK);
+		addHandler(new ChangeHandler() {
+			@Override
+			public void onChange(ChangeEvent event) {
+				if (iconElement != null) {
+					iconElement.removeFromParent();
+				}
+				if (error && iconType != null) {
+					iconElement = createIconElement();
+					getElement().insertFirst(iconElement);
+				}
+			}
+		}, ChangeEvent.getType());
+	}
 
-    /**
-     * @return the icon type
-     */
-    public IconType getIconType() {
-        return iconType;
-    }
+	/**
+	 * Clear the error state of this help block.
+	 */
+	public void clearError() {
+		error = false;
+		setText("");
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    public void setHTML(String html) {
-        super.setHTML(html);
-        DomEvent.fireNativeEvent(Document.get().createChangeEvent(), this);
-    }
+	/**
+	 * @return a new icon element. We only create this when {@link #iconElement}
+	 *         is null or the {@link #iconType} has changed.
+	 */
+	protected Element createIconElement() {
+		Element e = Document.get().createElement(ElementTags.I);
+		e.addClassName(Styles.FONT_AWESOME_BASE);
+		e.addClassName(iconType.getCssName());
+		e.getStyle().setPaddingRight(5, Unit.PX);
+		return e;
+	}
 
-    /**
-     * Sets the icon type. If the icon type changes programatically then the icon is removed from the dom and
-     * recreated.
-     *
-     * @param type the new icon type
-     */
-    public void setIconType(IconType type) {
-        IconType prevType = iconType;
-        iconType = type;
-        if (iconType != prevType && iconElement != null) {
-            iconElement.removeFromParent();
-            iconElement = null;
-            DomEvent.fireNativeEvent(Document.get().createChangeEvent(), this);
-        }
-    }
+	/**
+	 * @return the icon type
+	 */
+	public IconType getIconType() {
+		return iconType;
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    public void setText(String text) {
-        super.setText(text);
-        DomEvent.fireNativeEvent(Document.get().createChangeEvent(), this);
-    }
+	/**
+	 * Checks if this block is in the error state.
+	 *
+	 * @return true, if is error
+	 */
+	public boolean isError() {
+		return error;
+	}
+
+	/**
+	 * Set this {@link HelpBlock}'s error state.
+	 * 
+	 * @param message
+	 *            the error message.
+	 */
+	public void setError(String message) {
+		error = true;
+		setText(message);
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public void setHTML(String html) {
+		super.setHTML(html);
+		DomEvent.fireNativeEvent(Document.get().createChangeEvent(), this);
+	}
+
+	/**
+	 * Sets the icon type. If the icon type changes programatically then the
+	 * icon is removed from the dom and recreated.
+	 *
+	 * @param type
+	 *            the new icon type
+	 */
+	public void setIconType(IconType type) {
+		IconType prevType = iconType;
+		iconType = type;
+		if (iconType != prevType && iconElement != null) {
+			iconElement.removeFromParent();
+			iconElement = null;
+			DomEvent.fireNativeEvent(Document.get().createChangeEvent(), this);
+		}
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public void setText(String text) {
+		super.setText(text);
+		DomEvent.fireNativeEvent(Document.get().createChangeEvent(), this);
+	}
 
 }
