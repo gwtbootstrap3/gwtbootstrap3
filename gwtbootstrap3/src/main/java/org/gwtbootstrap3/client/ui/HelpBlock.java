dd57f1c713ff1f4caa23f6d61249097312809f8f
Steven Jardine <steve@mjnservices.com>
2015-07-28 11:06:14 -0600

    Fixed checkstyle problem.

--- gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/HelpBlock.java
+++ gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/HelpBlock.java
@@ -41,108 +41,108 @@ import com.google.gwt.event.dom.client.DomEvent;
  */
 public class HelpBlock extends AbstractTextWidget {
 
-	private boolean error = false;
+    private boolean error = false;
 
-	private Element iconElement = null;
+    private Element iconElement = null;
 
-	private IconType iconType = null;
+    private IconType iconType = null;
 
-	/**
-	 * Constructor.
-	 */
-	public HelpBlock() {
-		super(Document.get().createSpanElement());
-		setStyleName(Styles.HELP_BLOCK);
-		addHandler(new ChangeHandler() {
-			@Override
-			public void onChange(ChangeEvent event) {
-				if (iconElement != null) {
-					iconElement.removeFromParent();
-				}
-				if (error && iconType != null) {
-					iconElement = createIconElement();
-					getElement().insertFirst(iconElement);
-				}
-			}
-		}, ChangeEvent.getType());
-	}
+    /**
+     * Constructor.
+     */
+    public HelpBlock() {
+        super(Document.get().createSpanElement());
+        setStyleName(Styles.HELP_BLOCK);
+        addHandler(new ChangeHandler() {
+            @Override
+            public void onChange(ChangeEvent event) {
+                if (iconElement != null) {
+                    iconElement.removeFromParent();
+                }
+                if (error && iconType != null) {
+                    iconElement = createIconElement();
+                    getElement().insertFirst(iconElement);
+                }
+            }
+        }, ChangeEvent.getType());
+    }
 
-	/**
-	 * Clear the error state of this help block.
-	 */
-	public void clearError() {
-		error = false;
-		setText("");
-	}
+    /**
+     * Clear the error state of this help block.
+     */
+    public void clearError() {
+        error = false;
+        setText("");
+    }
 
-	/**
-	 * @return a new icon element. We only create this when {@link #iconElement}
-	 *         is null or the {@link #iconType} has changed.
-	 */
-	protected Element createIconElement() {
-		Element e = Document.get().createElement(ElementTags.I);
-		e.addClassName(Styles.FONT_AWESOME_BASE);
-		e.addClassName(iconType.getCssName());
-		e.getStyle().setPaddingRight(5, Unit.PX);
-		return e;
-	}
+    /**
+     * @return a new icon element. We only create this when {@link #iconElement}
+     *         is null or the {@link #iconType} has changed.
+     */
+    protected Element createIconElement() {
+        Element e = Document.get().createElement(ElementTags.I);
+        e.addClassName(Styles.FONT_AWESOME_BASE);
+        e.addClassName(iconType.getCssName());
+        e.getStyle().setPaddingRight(5, Unit.PX);
+        return e;
+    }
 
-	/**
-	 * @return the icon type
-	 */
-	public IconType getIconType() {
-		return iconType;
-	}
+    /**
+     * @return the icon type
+     */
+    public IconType getIconType() {
+        return iconType;
+    }
 
-	/**
-	 * Checks if this block is in the error state.
-	 *
-	 * @return true, if is error
-	 */
-	public boolean isError() {
-		return error;
-	}
+    /**
+     * Checks if this block is in the error state.
+     *
+     * @return true, if is error
+     */
+    public boolean isError() {
+        return error;
+    }
 
-	/**
-	 * Set this {@link HelpBlock}'s error state.
-	 * 
-	 * @param message
-	 *            the error message.
-	 */
-	public void setError(String message) {
-		error = true;
-		setText(message);
-	}
+    /**
+     * Set this {@link HelpBlock}'s error state.
+     * 
+     * @param message
+     *            the error message.
+     */
+    public void setError(String message) {
+        error = true;
+        setText(message);
+    }
 
-	/** {@inheritDoc} */
-	@Override
-	public void setHTML(String html) {
-		super.setHTML(html);
-		DomEvent.fireNativeEvent(Document.get().createChangeEvent(), this);
-	}
+    /** {@inheritDoc} */
+    @Override
+    public void setHTML(String html) {
+        super.setHTML(html);
+        DomEvent.fireNativeEvent(Document.get().createChangeEvent(), this);
+    }
 
-	/**
-	 * Sets the icon type. If the icon type changes programatically then the
-	 * icon is removed from the dom and recreated.
-	 *
-	 * @param type
-	 *            the new icon type
-	 */
-	public void setIconType(IconType type) {
-		IconType prevType = iconType;
-		iconType = type;
-		if (iconType != prevType && iconElement != null) {
-			iconElement.removeFromParent();
-			iconElement = null;
-			DomEvent.fireNativeEvent(Document.get().createChangeEvent(), this);
-		}
-	}
+    /**
+     * Sets the icon type. If the icon type changes programatically then the
+     * icon is removed from the dom and recreated.
+     *
+     * @param type
+     *            the new icon type
+     */
+    public void setIconType(IconType type) {
+        IconType prevType = iconType;
+        iconType = type;
+        if (iconType != prevType && iconElement != null) {
+            iconElement.removeFromParent();
+            iconElement = null;
+            DomEvent.fireNativeEvent(Document.get().createChangeEvent(), this);
+        }
+    }
 
-	/** {@inheritDoc} */
-	@Override
-	public void setText(String text) {
-		super.setText(text);
-		DomEvent.fireNativeEvent(Document.get().createChangeEvent(), this);
-	}
+    /** {@inheritDoc} */
+    @Override
+    public void setText(String text) {
+        super.setText(text);
+        DomEvent.fireNativeEvent(Document.get().createChangeEvent(), this);
+    }
 
 }
