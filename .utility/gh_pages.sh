0c1ed7f1cae28866c129028bf44017d958a8230f
Steven Jardine <steve@mjnservices.com>
2015-10-26 17:34:22 -0600

    More work on automatic push demo and javadoc.

--- .utility/gh_pages.sh
+++ .utility/gh_pages.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -ev
 if [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-	
+
+if [[ -z "$GH_TOKEN" ]]; then
+echo -e "GH_TOKEN is not set"
+exit 1
+fi	
+			
 echo -e "Publishing snapshot demo app and javadoc . . .\n"
 
 cp -R ./gwtbootstrap3/target/javadoc $HOME/javadoc-latest
@@ -11,13 +16,13 @@ git config --global user.name "travis-ci"
 
 # clone and build the demo application.
 cd $HOME
-git clone --quiet --branch=master https://${GH_TOKEN}@github.com/gwtbootstrap3/gwtbootstrap3-demo demo > /dev/null
+git clone --quiet --branch=master https://$GH_TOKEN@github.com/gwtbootstrap3/gwtbootstrap3-demo demo > /dev/null
 cd demo
 mvn clean package
 
 # clone the gh-pages branch.
 cd $HOME
-git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/gwtbootstrap3/gwtbootstrap3-demo gh-pages > /dev/null
+git clone --quiet --branch=gh-pages https://$GH_TOKEN@github.com/gwtbootstrap3/gwtbootstrap3-demo gh-pages > /dev/null
 cd gh-pages
 
 # remove the GwtBootstrap3Demo and apidocs directories from git.
