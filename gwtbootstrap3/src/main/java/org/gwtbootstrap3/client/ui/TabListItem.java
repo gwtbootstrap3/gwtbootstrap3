973daee28913e4352ca9f3846acf41af73c6ca17
Steven Jardine <steve@mjnservices.com>
2015-12-16 10:04:30 -0700

    Fixed some comments.

--- gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/TabListItem.java
+++ gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/TabListItem.java
@@ -4,7 +4,7 @@ package org.gwtbootstrap3.client.ui;
  * #%L
  * GwtBootstrap3
  * %%
- * Copyright (C) 2013 - 2014 GwtBootstrap3
+ * Copyright (C) 2013 - 2015 GwtBootstrap3
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -138,43 +138,37 @@ public class TabListItem extends AnchorListItem implements HasDataTarget {
         setDataTarget(href);
     }
 
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
     @Override
     public String getHref() {
         return getDataTarget();
     }
 
+    /** {@inheritDoc} */
     @Override
     public void setDataTargetWidgets(final List<Widget> widgets) {
         anchor.setDataTargetWidgets(widgets);
     }
 
+    /** {@inheritDoc} */
     @Override
     public void setDataTargetWidget(final Widget widget) {
         anchor.setDataTargetWidget(widget);
     }
 
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
     @Override
     public void setDataTarget(final String dataTarget) {
         anchor.setDataTarget(dataTarget);
     }
 
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
     @Override
     public String getDataTarget() {
         return anchor.getDataTarget();
     }
 
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
     @Override
     public void setEnabled(final boolean enabled) {
         super.setEnabled(enabled);
@@ -187,9 +181,7 @@ public class TabListItem extends AnchorListItem implements HasDataTarget {
         }
     }
 
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
     @Override
     protected void onLoad() {
         super.onLoad();
@@ -198,9 +190,7 @@ public class TabListItem extends AnchorListItem implements HasDataTarget {
         bindJavaScriptEvents(anchor.getElement());
     }
 
-    /**
-     * {@inheritDoc}
-     */
+    /** {@inheritDoc} */
     @Override
     protected void onUnload() {
         super.onUnload();
@@ -261,4 +251,5 @@ public class TabListItem extends AnchorListItem implements HasDataTarget {
         $wnd.jQuery(e).off('show.bs.tab');
         $wnd.jQuery(e).off('shown.bs.tab');
     }-*/;
+    
 }
