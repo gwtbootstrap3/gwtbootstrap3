449335e618730f81ab6dc73845830538cf67f6e7
root <root@localhost.localdomain>
2019-09-10 14:35:39 +0530

    Upgrade bootstrap to version 3.4.1 for fixing XSS vulnerability

--- gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/GwtBootstrap3ClientBundle.java
+++ gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/GwtBootstrap3ClientBundle.java
@@ -37,6 +37,6 @@ public interface GwtBootstrap3ClientBundle extends ClientBundle {
     @Source("resource/js/jquery-1.12.4.min.cache.js")
     TextResource jQuery();
 
-    @Source("resource/js/bootstrap-3.4.0.min.cache.js")
+    @Source("resource/js/bootstrap-3.4.1.min.cache.js")
     TextResource bootstrap();
 }
